88c39303a723e088a2f82dcacef782b0c1cb3f15
wilmerbacca1991 <wilmer.bacca1991@gmail.com>
2025-10-07 20:51:32 -0400

    updated file in class

---
+++ scripts/script.js
@@ -0,0 +1,55 @@
+//External Javascript//
+
+let c=a+b;
+
+let name = "Wilmer";
+
+name= "Wilmer Bacca";
+name= "Wilmer-Jose Bacca Zeepeda";
+
+let eligibility = false; //BOOLEAN//
+
+let bug = null; //Do not use reserved names for variables like let, null, error etc.//
+
+let firstname = "Wilmer";
+let LastName = "Bacca";  //Camel casing//
+
+//Console log to display messages for testing//
+console.log(c);
+console.log("My full name is "+firstname+LastName);
+console.log(LastName);
+console.log(typeof(eligibility));
+
+//Javascript cannot add a string and a number it will convert it automatically//
+
+//Variable naming//
+let name1; let _name; let name$;
+// never start variable name with a number, not allowed//
+
+
+//CONSTANTS
+const PI = 3.14;
+const GRAVITY_OF_EARTH = 9.8; //SCREAMING SNAKE CASE
+
+
+let radius = 2;
+
+let area = PI * (radius**2);
+
+let modulo = 37%10; //remainder of division operator//
+modulo=c%a;
+
+let age=24;
+eligibilty = age>18; // is 24>18 ? it's true so boolean
+
+
+
+
+
+
+
+
+
+
+
+
